a2f44030be59836ee957f6ad7de4fe4486940de6
Morgan Nelson <mnelson@steele-forge.com>
2015-05-12 15:12:17 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures squid as a simple caching proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.2"
+name             "squid"
 
 %w{ debian ubuntu centos fedora redhat scientific suse amazon}.each do |os|
   supports os
